cca7e0fb817cd70be01fe0e6e492d92b8407aba9
Eric Born <eric.born85@gmail.com>
2019-09-06 13:20:19 -0700

    Add files via upload

---
+++ Locations by county.sql
@@ -0,0 +1,29 @@
+select * from location_mstr
+order by location_name
+
+--***San Diego***
+ '4A785292-DBEB-4D9F-B80A-49E0F7B4999A'		--Carlsbad Planned Parenthood
+,'0565487A-C88D-484C-9759-3DF762EA0695'		--Chula Vista Planned Parenthood
+,'782C0260-7552-426E-87D6-38F073F40DAD'		--City Heights Planned Parenthood
+,'A0D201B2-7AD9-40DD-8A0B-F270478B1736'		--College Ave Planned Parenthood
+,'9EA2DE96-E929-499E-819B-4128A72CBC7B'		--El Cajon Planned Parenthood
+,'6FAF7F6A-0424-41B0-8B13-D2678C76898A'		--Escondido Planned Parenthood
+,'DA5FCD52-AFBE-47F9-A2A2-D96601252CDF'		--Euclid Ave Planned Parenthood
+,'6CB12D65-A88C-405C-89C0-7FE677C9D638'		--FA Family Planning Planned Parent
+,'68C7DDB4-834A-4ABC-B3EB-87BF71D60F41'		--FA Surgical Services Planned Pare
+,'05483D36-4D7C-49B7-8FF1-7AE9FA0E2825'		--Kearny Mesa Planned Parenthood
+,'D89E78A1-F4E4-4DC5-8A7A-BCC316E3A747'		--Mira Mesa Planned Parenthood
+,'239055AA-220B-4F4B-BC82-0A40825A2F2C'		--Mission Bay Planned Parenthood
+,'8CA933B0-8657-4B7C-8B03-F54916ED5103'		--MV Express Planned Parenthood
+,'9B706667-0AC8-4BAB-AC72-63B0075D05BF'		--Vista Planned Parenthood
+,'5E5D55D7-22BC-40C0-A307-E1CD056CC782'		--Pacific Beach Express Planned Parenthood
+
+--***Riverside***
+'ACB96567-0B1F-4AF7-81FC-598B26C3E3DC'		--Coachella Valley Planned Parenthood
+,'4BD8BD13-6076-4C78-AC9E-FEEC37F226D5'		--Moreno Valley Planned Parenthood
+,'2E863B41-F3B9-4768-AC31-AA300DAA9003'		--Rancho Mirage Planned Parenthood
+,'C1CAF54E-57B5-4A9F-84E7-554A8EF4EADB'		--Riverside Planned Parenthood
+,'E53D4FEC-7778-4093-9C45-DC526C9CC8D3'		--RS Surgical Services Planned Parenthood
+
+--***Imperial Valley***
+'5C8A71F3-7496-4C4F-86E8-AEE02ADECCF4'		--Imperial Valley Planned Parenthood
